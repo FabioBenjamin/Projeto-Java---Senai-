d23a63e9be100f0ce11c12517ee3c9e51ecb84c1
Fabio Benjamin Oliveira <guten.abend245791@gmail.com>
2026-01-27 23:40:45 -0300

    Add files via upload

---
+++ SENAI/Projeto_Senai_Java/src/java2026/Teste27012026.java
@@ -0,0 +1,14 @@
+package java2026;
+
+public class Teste27012026 {
+    public static void main(String[] args) {
+
+        long x = 1;
+        long y = 1;
+        long z = 0;
+
+        z =+ x;
+
+        System.out.println(z);
+    }
+}
