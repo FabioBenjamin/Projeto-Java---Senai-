4ad8c435e8ce7716009630a37cfb202529749c43
Fabio Benjamin Oliveira <guten.abend245791@gmail.com>
2026-02-16 16:09:11 -0300

    Update CalculadoraController.java

--- SENAI/Projeto_Senai_Java/Calculadora/src/main/java/com/calculadora/controller/CalculadoraController.java
+++ SENAI/Projeto_Senai_Java/Calculadora/src/main/java/com/calculadora/controller/CalculadoraController.java
@@ -1,49 +1,35 @@
-package main.java.com.calculadora.controller;
+package com.calculadora.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.calculadora.model.*;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 import java.util.Map;
 
 @RestController
 public class CalculadoraController {
 
-    @PostMapping("/calcular") // Define que este metodo vai ser executado
-    public Map<String, Object>
-    calcular(@RequestBody Map<String, String>corpo) {
+    @PostMapping("/calcular")
+    public Map<String, Object> calcular(@RequestBody Map<String, String> corpo) {
 
-        String expressao = corpo.get("expressao"); // Recebe do Js
-        double resultado = 0;
-
-        // Verifição de expressão
+        // Recebe a string vinda do Js
+        String expressao = corpo.get("expressao");
+        
         try {
-            if (expressao.contains("+")) {
-                String[] partes = expressao.split("\\+");
-                // Conversão de String para Real
-                resultado = new soma(Double.parseDouble(partes[0]), Double.parseDouble(partes[1])).executar();
+            // ExpressionBuilder monta a conta
+            Expression e = new ExpressionBuilder(expressao).build();
+            
+            double resultado = e.evaluate();
 
-            } else if (expressao.contains("-")) {
-                String[] partes = expressao.split("-");
-                // Conversão de String para Real
-                resultado = new Subtracao(Double.parseDouble(partes[0]), Double.parseDouble(partes[1])).executar();
-
-            } else if (expressao.contains("*")) {
-                String[] partes = expressao.split("\\*");
-                // Conversão de String para Real
-                resultado = new Multiplicacao(Double.parseDouble(partes[0]), Double.parseDouble(partes[1])).executar();
-
-            } else if (expressao.contains("/")) {
-                String[] partes = expressao.split("/");
-                // Conversão de String para Real
-                resultado = new Divisao(Double.parseDouble(partes[0]), Double.parseDouble(partes[1])).executar();
-            }
-
-            // Retorna para o Js
+            // Retorna o resultado para o Js
             return Map.of("resultado", resultado);
 
         }
-
-        // Tratamento de erro
+        // Tratamento de erro 
         catch (Exception e) {
-            return Map.of("resultado", "Erro");
-
+            return Map.of("resultado", "Erro matemático");
         }
     }
 }
