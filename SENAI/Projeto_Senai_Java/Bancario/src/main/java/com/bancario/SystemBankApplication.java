343355730822e61edf8f6f108cc79358249212ff
Fabio Benjamin Oliveira <guten.abend245791@gmail.com>
2026-02-22 18:59:38 -0300

    Refactor SystemBankApplication to load initial screen
    Updated the application to load the initial screen from 'telaIntrodutoria.fxml' and changed the window title to 'Sistema Bancário'.

--- SENAI/Projeto_Senai_Java/Bancario/src/main/java/com/bancario/SystemBankApplication.java
+++ SENAI/Projeto_Senai_Java/Bancario/src/main/java/com/bancario/SystemBankApplication.java
@@ -5,16 +5,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class SystemBankApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Aberte");
+    public void start(Stage stage) throws Exception {
+
+        // Tela inicial
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/telaIntrodutoria.fxml")
+        );
+
+        // Carregar tela inicial 
+        Scene scene = new Scene(loader.load());
+
+        stage.setTitle("Sistema Bancário");
         stage.setScene(scene);
         stage.show();
     }
-}
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
