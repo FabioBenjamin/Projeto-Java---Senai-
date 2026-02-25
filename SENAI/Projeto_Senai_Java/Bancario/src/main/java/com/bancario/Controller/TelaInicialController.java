fdce6b8a9ac86da0c383c4697ab37fd8847b9a73
Fabio Benjamin Oliveira <guten.abend245791@gmail.com>
2026-02-24 22:04:28 -0300

    Refactor TelaInicialController to load new view
    Updated TelaInicialController to load telaOperadores.fxml and pass Conta instance to TelaOperacionalController.

--- SENAI/Projeto_Senai_Java/Bancario/src/main/java/com/bancario/Controller/TelaInicialController.java
+++ SENAI/Projeto_Senai_Java/Bancario/src/main/java/com/bancario/Controller/TelaInicialController.java
@@ -1,27 +1,39 @@
 package com.bancario.Controller;
 
+import com.models.Conta;
 import javafx.event.ActionEvent;
-import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.Node;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class TelaInicialController {
 
+    private Conta conta;
+
     @FXML
     private void telasInicias (ActionEvent event) throws IOException {
 
-        Parent root = FXMLLoader.load(
-            getClass().getResource("/telaIntrodutoria.fmxl")
+        FXMLLoader loader = new FXMLLoader(
+            getClass().getResource("/views/telaOperadores.fxml")
         );
+
+        Parent root = loader.load();
+
+        TelaOperacionalController controller = loader.getController();
+        controller.setConta(conta);
+
 
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
         stage.show();
+
+
     }
 
 }
