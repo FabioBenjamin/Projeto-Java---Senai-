90e11d0cccc3ce352aacfc6838b9bda92517585b
Fabio Benjamin Oliveira <guten.abend245791@gmail.com>
2026-02-24 22:04:05 -0300

    Refactor TelaOperacionalController for deposit and withdraw
    Implementação
    ajustes nos operadores

--- SENAI/Projeto_Senai_Java/Bancario/src/main/java/com/bancario/Controller/TelaOperacionalController.java
+++ SENAI/Projeto_Senai_Java/Bancario/src/main/java/com/bancario/Controller/TelaOperacionalController.java
@@ -1,27 +1,111 @@
 package com.bancario.Controller;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+// Models
 import com.models.Conta;
+import com.models.deposito;
+import com.models.saque;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 
-public class TelaOperacionalController {
+public class TelaOperacionalController extends TelaInicialController{
 
     @FXML
-    private TextField campoValor;
+    private TextField Valor;
 
+    @FXML
+    private Label Saldo;
+
+    public void setConta(Conta conta) {
+        this.conta = conta;
+    }
+
+    // Trocador de telas
+    private Conta conta;
+
+    // Deposito
     @FXML
     private void depositar() {
-        double valor = Double.parseDouble(campoValor.getText());
-        Conta.depositar(valor);
+        double valor = Double.parseDouble(Valor.getText());
+
+        // Operação de deposito
+        deposito dep = new deposito(valor, conta);
+
+        atualizarSaldo();
+        Valor.clear();
+    }
+
+    // Saque
+    @FXML
+    private void sacar() {
+        double valor = Double.parseDouble(Valor.getText());
+
+        // Operação de saque
+        new saque(valor, conta);
+
+        atualizarSaldo();
+        Valor.clear();
     }
 
     @FXML
-    private void sacar(){
-        double valor = Double.parseDouble((campoValor.getText()));
+    private void saldo() {
+        double valor = Double.parseDouble(Valor.getText());
 
-        if(!Conta.sacar(valor)) {
-            System.out.println("Saldo insuficiente");
-        }
+        // Operação de saldo
+        atualizarSaldo();
+    }
+
+    // Saldo
+    @FXML
+    private void ExibirSaldo() {
+        atualizarSaldo();
+    }
+
+    // Atualização do saldo da conta pós operação
+    private void atualizarSaldo() {
+        Saldo.setText("Saldo: R$ " + conta.getSaldo());
+    }
+
+    // Metodo de implementação
+    private void trocarTela(ActionEvent event, String trocasTelas) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(trocasTelas));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource())
+                .getScene()
+                .getWindow();
+
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    // CONECTIVIDADE DAS TELAS
+
+    // DEPOSITO
+    @FXML
+    private void abrirDeposito(ActionEvent event) throws IOException {
+        trocarTela(event, "/views/telaDeposito.fxml");
+    }
+
+    // SAQUE
+    @FXML
+    private void abrirSaque(ActionEvent event) throws IOException {
+        trocarTela(event, "/views/telaSaque.fxml");
+    }
+
+    // SALDO
+    @FXML
+    private void abrirSaldo(ActionEvent event) throws IOException {
+        trocarTela(event, "/views/telaSaldo.fxml");
     }
 }
