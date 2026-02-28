57629b5fa74735aa02a5db0b38c73013b7344f78
Fabio Benjamin Oliveira <guten.abend245791@gmail.com>
2026-02-28 15:54:02 -0300

    Remove unused Conta variable from TelaInicialController

--- SENAI/Projeto_Senai_Java/Bancario/src/main/java/com/bancario/Controller/TelaInicialController.java
+++ SENAI/Projeto_Senai_Java/Bancario/src/main/java/com/bancario/Controller/TelaInicialController.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 
 public class TelaInicialController {
 
-    private Conta conta;
 
     @FXML
     private void telasInicias (ActionEvent event) throws IOException {
@@ -25,15 +24,11 @@ public class TelaInicialController {
 
         Parent root = loader.load();
 
-        TelaOperacionalController controller = loader.getController();
-        controller.setConta(conta);
-
 
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
         stage.show();
-
-
     }
 
 }
+
