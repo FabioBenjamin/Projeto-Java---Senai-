5e114ed865a310581e9aee4f3daf43be792f8488
Fabio Benjamin Oliveira <guten.abend245791@gmail.com>
2026-02-28 15:59:04 -0300

    Adição no module-info

--- SENAI/Projeto_Senai_Java/Bancario/src/main/java/module-info.java
+++ SENAI/Projeto_Senai_Java/Bancario/src/main/java/module-info.java
@@ -1,12 +1,16 @@
 module com.example.bancario {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.example.bancario to javafx.fxml;
-    exports com.example.bancario;
-    exports com.bancario;
+    // Acesso aos models
+    exports com.models;
+    opens com.models to javafx.fxml;
+
+    // Controllers
     opens com.bancario to javafx.fxml;
+    exports com.bancario;
+    exports com.bancario.Controller;
+    opens com.bancario.Controller to javafx.fxml;
 }
